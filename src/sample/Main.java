50c140acd65d9b48cc7cfed8ada8922b38fee2dc
john <johnmakola@gmail.com>
2020-06-03 13:21:47 +0300

    With Table

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,13 +16,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
+import javafx.stage.WindowEvent;
 
 
 public class Main extends Application {
     Stage Window;
     TableView<Products> table1;
-
+    TextField Name,Price,Quanity;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -44,11 +44,32 @@ public class Main extends Application {
         namePrice.setMinWidth(100);
         namePrice.setCellValueFactory(new PropertyValueFactory<>("price"));
 
-
         //Name Colum
         TableColumn<Products,Integer> nameQuantity = new TableColumn<>("Quantity");
         nameQuantity.setMinWidth(200);
         nameQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        //Add Name Input
+        Name = new TextField();
+        Name.setPromptText("Name");
+        Name.setMinWidth(20);
+
+        //Add Price Input
+       Price = new TextField();
+        Price.setPromptText("Price");
+
+        //Add Quantity  Input
+        Quanity = new TextField();
+        Quanity.setPromptText("Quantity");
+
+       //Add Button Delete Buttopn
+        Button addButton = new Button("Add");
+        addButton.setOnAction(e -> AddButtonClicked ());
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(e -> deleteButtonClicked ());
+        Button QuitButton = new Button("Quit");
+        QuitButton.setOnAction(e-> closeProgram());
+
 
         table1 = new TableView<>();
         table1.setItems(getProduct());
@@ -56,8 +77,20 @@ public class Main extends Application {
 
 
         // Set Layout
-        StackPane Layout = new StackPane();
-        Layout.getChildren().add(table1);
+        HBox HB = new HBox();
+        HB.setPadding(new Insets (10,10,10,10 ));
+        HB.setSpacing(10);
+        HB.getChildren().addAll(Name,Price,Quanity,addButton,deleteButton);
+
+        HBox HB1 = new HBox();
+        HB1.setPadding(new Insets (10,10,10,10 ));
+        HB1.setSpacing(10);
+        HB1.getChildren().add(QuitButton);
+
+
+        VBox Layout = new VBox();
+        Layout.getChildren().addAll(table1,HB,HB1);
+
 
         // Scene
         Scene scene = new Scene(Layout, 300, 400);
@@ -71,6 +104,35 @@ public class Main extends Application {
         launch(args);
     }
 
+    //Close Program
+    public void closeProgram(){
+        Window.close();
+    }
+    // Add Button Functionality
+
+    public void AddButtonClicked(){
+        Products prod = new Products();
+        prod.setName(Name.getText());
+        prod.setPrice(Double.parseDouble (Price.getText()));
+        prod.setQuantity(Integer.parseInt(Quanity.getText()));
+        table1.getItems().addAll(prod);
+        Name.clear();
+        Price.clear();
+        Quanity.clear();
+
+    }
+
+
+    // Delete Button
+    public void deleteButtonClicked(){
+        ObservableList<Products> prodSelected, allProducts;
+        allProducts = table1.getItems();
+        prodSelected = table1.getSelectionModel().getSelectedItems();
+
+        prodSelected.forEach(allProducts::remove);
+
+
+    }
 
 // Get all of the Products
 
